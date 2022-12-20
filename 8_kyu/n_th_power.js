d5fc62f173c750b17e92736948315f2f5a27dff3
DabTheMatt <inspiromaciek@gmail.com>
2022-12-20 21:21:00 +0100

    fix

--- 8_kyu/n_th_power.js
+++ 8_kyu/n_th_power.js
@@ -2,7 +2,11 @@ let array = [4, 6, 9];
 let n = 3;
 
 function index(array, n){
-    return Math.pow(array[n], n);
+    if (n>=array.length) {
+        return -1;
+    } else {
+        return Math.pow(array[n], n);
+    }
   }
 
 console.log(index(array, n));
