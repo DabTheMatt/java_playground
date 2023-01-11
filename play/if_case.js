17381b3909ba2a1725dcca90333f5922944da45a
DabTheMatt <inspiromaciek@gmail.com>
2023-01-11 20:05:22 +0100

    gab_2

---
+++ play/if_case.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+// for (let i = 0; i < 9; i++) { // i - iterator
+//   console.log(i)
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i, ' Ala')
+// }
+
+
+// for (let i = 0; i < 15; i++) {
+//     console.log('sukces');
+// }
+
+
+
+// let numberOne = 5;
+// let numberTwo = 7;
+
+// numberOne -= 3; // numberOne = numberOne -3;
+
+// console.log('n1', numberOne);
+
+// numberTwo += 5; // numberTwo = numberTwo + 5;
+
+// console.log('n2', numberTwo);
+
+// numberOne++; // inkrementacja, zwiększenie wartości o 1;
+// numberTwo--; // dekrementacja, zmniejszenie wartości o 1;
+
+// console.log('to miejsce: ', numberTwo);
---
+++ play/name_for.js
@@ -0,0 +1,11 @@
+
+
+let personAge = 7;
+
+if (personAge >= 18) {
+    for (let i=0; i < personAge; i++) {
+        console.log(personAge)
+    }
+} else if (personAge < 18) {
+    console.log('You are to young!')
+}
