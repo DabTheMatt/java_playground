f5f13f80c8deddd6cfa80ba053db8da299e4694f
DabTheMatt <inspiromaciek@gmail.com>
2023-01-13 20:25:38 +0100

    1_8k

---
+++ 8_kyu/lightsabers.js
@@ -0,0 +1,16 @@
+// function howManyLightsabersDoYouOwn_2(name) {
+//     let lighsabers;
+
+//     if (name === "Zach") {
+//        lighsabers = 18;
+//     } else {
+//         lighsabers = 0;
+//     }
+//     return lighsabers;
+// }
+
+// console.log(howManyLightsabersDoYouOwn_2('Zach'));
+
+howManyLight = ownerName => ownerName === 'Zach' ? 18 : 0;
+
+console.log(howManyLight('Piotrek'));
